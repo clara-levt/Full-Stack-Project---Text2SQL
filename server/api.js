c199be43deadb6af2c896ff2369b258674f212b8
clara-levt <clara.levt@gmail.com>
2023-10-15 22:43:58 +0700

    "setup openai backend"

--- server/api.js
+++ server/api.js
@@ -6,4 +6,12 @@ const openaiApiKey = process.env.OPENAI_API_KEY
 
 if (!openaiApiKey) {
     console.error('OPENAI_API_KEY is not set')
+    process.exit(1)
 }
+
+const configuration = new Configuration({
+    apiKey: openaiApiKey
+})
+const openai = new OpenAIApi(configuration)
+
+export default openai
