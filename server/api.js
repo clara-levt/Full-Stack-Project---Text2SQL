2f8e1480f2dfa6b84b0fbde34d31060444ef2f22
clara-levt <clara.levt@gmail.com>
2023-10-15 22:18:20 +0700

    try to commit to git

---
+++ server/api.js
@@ -0,0 +1,9 @@
+import {Configuration, OpenAIApi} from "openai";
+import dotenv from "dotenv"
+dotenv.config();
+
+const openaiApiKey = process.env.OPENAI_API_KEY
+
+if (!openaiApiKey) {
+    console.error('OPENAI_API_KEY is not set')
+}
